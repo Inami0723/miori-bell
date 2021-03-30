c89e4535550bd32621ebc0cd62cbd2c0889f91ee
Debian <bitnami@ip-172-26-13-121.ap-northeast-2.compute.internal>
2021-03-30 00:13:51 +0000

    init commit

---
+++ data/js/resources.js
@@ -0,0 +1,27 @@
+game.resources = [
+    {
+        "name": "bell",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "background",
+        "type": "image",
+        "src": "data/img/background.png"
+    },
+    {
+        "name": "texture",
+        "type": "image",
+        "src": "data/img/texture.png"
+    },
+    {
+        "name": "background",
+        "type": "json",
+        "src": "data/img/background.json"
+    },
+    {
+        "name": "texture",
+        "type": "json",
+        "src": "data/img/texture.json"
+    }
+];
