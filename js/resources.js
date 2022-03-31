519fce8d4aa879c12037758cb8e8424264847c9e
Inami0723 <inamiyui0723@gmail.com>
2022-04-01 05:10:21 +0900

    update to 2022 version

---
+++ js/resources.js
@@ -0,0 +1,37 @@
+game.resources = [
+    {
+        "name": "bell",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "squish",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "yoho",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "background",
+        "type": "image",
+        "src": "data/img/background.png"
+    },
+    {
+        "name": "texture",
+        "type": "image",
+        "src": "data/img/texture.png"
+    },
+    {
+        "name": "background",
+        "type": "json",
+        "src": "data/img/background.json"
+    },
+    {
+        "name": "texture",
+        "type": "json",
+        "src": "data/img/texture.json"
+    }
+];
